c4df385f8d06a126d02edcd35b311e793d372aa4
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2021-03-19 14:04:03 -0500

    Update ForecastServer.js

--- src/services/ForecastServer.js
+++ src/services/ForecastServer.js
@@ -1,16 +1,19 @@
 import axios from 'axios'
 
-
 const REST_API_URL = "http://localhost:8080/api/v1/forecast"
-
 
 class ForecastService {
     getInvestmentOptions() {
         return axios.get(REST_API_URL);    
     }
 
-    getForecast() {
-        return axios.post(REST_API_URL); 
+    getForecast(allocations) {
+        var req = JSON.stringify(allocations)
+        req = req.replace("FinancialServices", "Financial Services");
+        req = req.replace("RealEstate", "Real Estate");
+        console.log(req)
+        const request = JSON.parse(req)
+        return axios.post(REST_API_URL, request); 
     }
 }
 
